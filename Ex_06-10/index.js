cf49fa6b5b50729cba0863286fa8819aeaa9c8ea
Muhammar Ammar Masood <jazba685@gmail.com>
2023-07-12 09:41:06 +0500

    Ex-10

---
+++ Ex_06-10/index.js
@@ -0,0 +1,14 @@
+"use strict";
+//--------------------------------------EX-06----------------------------------------------------//
+let person_name = " \n    \t   Ammar \t \n";
+console.log(person_name);
+console.log(person_name.trim()); //display name after striping whitespaces
+//--------------------------------------EX-07,08----------------------------------------------------//
+console.log(3 + 5);
+console.log(10 - 2);
+console.log(2 * 4);
+console.log(16 / 2);
+//--------------------------------------EX-09,10----------------------------------------------------//
+let favNum = 5;
+console.log(`My Favourite Number is ${favNum}`);
+//These are Typescript Assignments
