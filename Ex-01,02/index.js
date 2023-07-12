a86ed1f138ce18bb648e703927d97ddc204284e9
Muhammar Ammar Masood <jazba685@gmail.com>
2023-07-12 09:09:03 +0500

    Ex_01-05

--- Ex-01,02/index.js
+++ Ex-01,02/index.js
@@ -1,3 +1,15 @@
 "use strict";
-let _name = "Ammar";
-console.log(`Hello ${_name}, would you like to learn some Python today?`);
+//--------------------------------------EX-01,02----------------------------------------------------//
+let name1 = "Ammar";
+console.log(`Hello ${name1}, would you like to learn some Python today?`);
+//--------------------------------------EX-03----------------------------------------------------//
+let name2 = "ammar";
+console.log(name2.toLocaleLowerCase()); //lowercase
+console.log(name2.toUpperCase()); //UPPERCASE
+console.log(name2.charAt(0).toUpperCase() + name2.slice(1)); //Titlecase
+//--------------------------------------EX-04----------------------------------------------------//
+console.log(`Prophet Muhammad(PBUH) said: "The best of you are those who are best in character."`);
+//--------------------------------------EX-05----------------------------------------------------//
+let famous_person = "Prophet Muhammad(PBUH)";
+let message = "The best of you are those who are best in character.";
+console.log(`${famous_person} said: "${message}"`);
